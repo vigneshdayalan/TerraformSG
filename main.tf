54cc0ab7c0f9ee91c4886db934fbb1b9fa44e586
Vignesh Babu <Vignesh.Babu@emishealth.com>
2021-07-27 21:39:39 +0530

    ip updated

--- main.tf
+++ main.tf
@@ -7,15 +7,15 @@ terraform {
       version = "3.51.0"
     }
   }
+
+backend "s3" {
+  bucket                  = "dev-securitygroups-public-terraformstate"
+  key                     = "sg-statefile"
+  region                  = "eu-west-2"
+  shared_credentials_file = "C:/Users/Vignesh.babu/.aws/credentials"
+  profile                 = "604575884205_generic-dev-access"
+  }
 }
-# backend "s3" {
-#   bucket                  = "dev-securitygroups-public-terraformstate"
-#   key                     = "sg-statefile"
-#   region                  = "eu-west-2"
-#   shared_credentials_file = "C:/Users/Vignesh.babu/.aws/credentials"
-#   profile                 = "cohortcore-dev"
-#   dynamodb_table          = "terraform-state-locktable"
-# }
 
 provider "aws" {
   region                  = "eu-west-2"
