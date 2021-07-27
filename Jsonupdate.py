4c23d66a137b867d9667e5ea14bcd3e74d1d7dc9
Vignesh Babu <Vignesh.Babu@emishealth.com>
2021-07-27 20:34:20 +0530

    code updated

--- Jsonupdate.py
+++ Jsonupdate.py
@@ -1,28 +1,20 @@
 import json
+import argparse, sys
+from requests import get
 
-# a_file = open("terraform.tfvars.json", "r")
-# json_object = json.load(a_file)
-# a_file.close()
-# print(json_object)
+parser=argparse.ArgumentParser()
 
-# json_object["sg_ingress_rules"]["Vignesh"] = 101
+parser.add_argument('--name', help='name of the user for which ip updated')
 
-# a_file = open("terraform.tfvars.json", "w")
-# json.dump(json_object, a_file,indent=4)
-# a_file.close()
-# print(json_object)
+args=parser.parse_args()
+filename='./resources/terraform.tfvars.json'
 
+ip = get('https://ipapi.co/ip/').text
 
-# function to add to JSON
-def update_jsonip(userdetails, filename='terraform.tfvars.json'):
-    with open(filename,'r+') as file:
+with open(filename,'r+') as file:
         file_data = json.load(file)
         print(file_data)
-        file_data["sg_ingress_rules"][userdetails["name"]] = userdetails["ip"]
+        file_data["sg_ingress_rules"][args.name.lower()] = ip
         file.seek(0)
-        json.dump(file_data, file, indent = 4,)
- 
-    # python object to be appended
-# sss
-userdetails = {"name":"Vignesh","ip":"200"}  
-update_jsonip(userdetails)
+        print(file_data)
+        json.dump(file_data, file, indent = 4)
